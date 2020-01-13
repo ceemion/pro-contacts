9d8ac4ddd49ca24ec8c7d46f7198cbe3c88ecd2e
ceemion <fiyinsimeon@gmail.com>
2020-01-13 17:47:39 +0100

    Initial Commit

---
+++ pro-contactsTests/pro_contactsTests.swift
@@ -0,0 +1,34 @@
+//
+//  pro_contactsTests.swift
+//  pro-contactsTests
+//
+//  Created by Fiyinfoluwa Adebayo on 13/01/2020.
+//  Copyright © 2020 Kompilab Limited. All rights reserved.
+//
+
+import XCTest
+@testable import pro_contacts
+
+class pro_contactsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
