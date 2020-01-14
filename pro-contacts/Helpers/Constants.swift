2ea5f68f7cdb3f7533723d22a135fe339779bb26
ceemion <fiyinsimeon@gmail.com>
2020-01-14 15:45:10 +0100

    Change launchscreen to full image bg

---
+++ pro-contacts/Helpers/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  pro-contacts
+//
+//  Created by Fiyinfoluwa Adebayo on 14/01/2020.
+//  Copyright © 2020 Kompilab Limited. All rights reserved.
+//
+
+import Foundation
